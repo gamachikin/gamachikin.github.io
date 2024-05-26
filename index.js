7da8b27d5a8a315a446772d586a697bb097686cf
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-05-26 16:24:30 -0400

    e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const skills = {
+        html: 80,
+        css: 60, 
+        js: 10
+    };
+
+    for (let skill in skills) {
+        const bar = document.getElementById(`${skill}-bar`);
+        const percentage = document.getElementById(`${skill}-percentage`);
+        const skillLevel = skills[skill];
+
+        bar.style.width = skillLevel + '%';
+        percentage.textContent = skillLevel + '%';
+    }
+});
 
